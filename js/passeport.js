ebe7383cfa8798ea3fb71d2a83cbdc3aac194c24
Gordon Beeman <40498309+thegordonbeeman@users.noreply.github.com>
2020-11-30 11:47:39 +0100

    Update passeport.js

--- js/passeport.js
+++ js/passeport.js
@@ -22,10 +22,26 @@ document.getElementById('agealea').onclick = function() {
   }
 }
 
+document.getElementById('domicilealea').onclick = function() {
+	if (this.checked) {
+  	document.getElementById('domicile').style.display ="none";
+  } else {
+  	document.getElementById('domicile').style.display ="block";
+  }
+}
+
 document.getElementById('delialea').onclick = function() {
 	if (this.checked) {
   	document.getElementById('deli').style.display ="none";
   } else {
   	document.getElementById('deli').style.display ="block";
+  }
+}
+
+document.getElementById('expalea').onclick = function() {
+	if (this.checked) {
+  	document.getElementById('exp').style.display ="none";
+  } else {
+  	document.getElementById('exp').style.display ="block";
   }
 }
