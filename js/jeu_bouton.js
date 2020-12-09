4b3b28ed1df9c4f0a21c2d4ac98d0e78c613abae
Gordon Beeman <40498309+thegordonbeeman@users.noreply.github.com>
2020-12-09 18:16:58 +0100

    Update jeu_bouton.js

--- js/jeu_bouton.js
+++ js/jeu_bouton.js
@@ -1,1 +1,19 @@
+var nombre = 0
 
+document.getElementById('commencer').onclick = function() {
+	document.getElementById('start').style.display = "none";
+  document.getElementById('jouer').style.display = "block";
+  
+  nombre = Math.floor(Math.random() * Math.floor(200));
+}
+
+document.getElementById('valider').onclick = function() {
+	var valeur = document.getElementById('entree').value;
+  if (valeur > nombre) {
+  	alert("Plus bas!")
+  } else if (valeur < nombre) {
+  	alert("Plus haut!")
+  } else {
+  	alert("BRAVO BG!")
+  }
+}
