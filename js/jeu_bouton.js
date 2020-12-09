bc387ae4ff6a731ed60353828d61ff2154db2d2d
Gordon Beeman <40498309+thegordonbeeman@users.noreply.github.com>
2020-12-09 18:41:00 +0100

    Update jeu_bouton.js

--- js/jeu_bouton.js
+++ js/jeu_bouton.js
@@ -11,9 +11,12 @@ function deviner() {
 	var valeur = document.getElementById('entree').value;
   if (valeur > nombre) {
   	alert("Plus bas!")
+	document.body.style.backgroundImage = "url('https://file1.telestar.fr/var/telestar/storage/images/3/1/5/0/3150739/vincent-lagaf-dans-strike.jpg?alias=original')"
   } else if (valeur < nombre) {
   	alert("Plus haut!")
+	document.body.style.backgroundImage = "url('https://www.premiere.fr/sites/default/files/styles/scale_crop_1280x720/public/2018-04/Audiences-TV-Le-juste-prix-de-Vincent-Lagaf-atteint-des-sommets.jpg')"
   } else {
   	alert("Tu as un qi de " + nombre)
+	document.body.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/785944479853051954/786286026606903326/telechargement.jpeg')"
   }
 }
