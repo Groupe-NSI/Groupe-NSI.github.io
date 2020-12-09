255690a4d3671848f088bac31caa98d25203ca5b
Gordon Beeman <40498309+thegordonbeeman@users.noreply.github.com>
2020-12-09 17:07:10 +0100

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,1 +1,4 @@
-
+document.getElementById('avertaccept').onclick = function() {
+	document.getElementById('avertissement').style.display = "none";
+  document.getElementById('contenu').style.display = "block";
+}
