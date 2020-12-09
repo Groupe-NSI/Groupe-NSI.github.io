9e1f99b8e63572399ba4b599e9872e5da9cc8b72
Gordon Beeman <40498309+thegordonbeeman@users.noreply.github.com>
2020-12-09 18:30:38 +0100

    Update jeu_bouton.js

--- js/jeu_bouton.js
+++ js/jeu_bouton.js
@@ -1,19 +1,19 @@
 var nombre = 0
 
-document.getElementById('commencer').onclick = function() {
+function commencer() {
 	document.getElementById('start').style.display = "none";
   document.getElementById('jouer').style.display = "block";
   
   nombre = Math.floor(Math.random() * Math.floor(200));
 }
 
-document.getElementById('valider').onclick = function() {
+function deviner() {
 	var valeur = document.getElementById('entree').value;
   if (valeur > nombre) {
   	alert("Plus bas!")
   } else if (valeur < nombre) {
   	alert("Plus haut!")
   } else {
-  	alert("BRAVO BG!")
+  	alert("Tu as un qi de " + nombre)
   }
 }
