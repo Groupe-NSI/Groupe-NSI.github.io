481f15fe87577c8748ffa781cd966c491d4581c6
Gordon Beeman <40498309+thegordonbeeman@users.noreply.github.com>
2020-12-09 18:43:33 +0100

    Update jeu_bouton.js

--- js/jeu_bouton.js
+++ js/jeu_bouton.js
@@ -10,13 +10,13 @@ function commencer() {
 function deviner() {
 	var valeur = document.getElementById('entree').value;
   if (valeur > nombre) {
-  	alert("Plus bas!")
-	document.body.style.backgroundImage = "url('https://file1.telestar.fr/var/telestar/storage/images/3/1/5/0/3150739/vincent-lagaf-dans-strike.jpg?alias=original')"
+	document.body.style.backgroundImage = "url('https://file1.telestar.fr/var/telestar/storage/images/3/1/5/0/3150739/vincent-lagaf-dans-strike.jpg?alias=original')";
+  	alert("Plus bas!");
   } else if (valeur < nombre) {
-  	alert("Plus haut!")
-	document.body.style.backgroundImage = "url('https://www.premiere.fr/sites/default/files/styles/scale_crop_1280x720/public/2018-04/Audiences-TV-Le-juste-prix-de-Vincent-Lagaf-atteint-des-sommets.jpg')"
+	document.body.style.backgroundImage = "url('https://www.premiere.fr/sites/default/files/styles/scale_crop_1280x720/public/2018-04/Audiences-TV-Le-juste-prix-de-Vincent-Lagaf-atteint-des-sommets.jpg')";
+  	alert("Plus haut!");
   } else {
-  	alert("Tu as un qi de " + nombre)
-	document.body.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/785944479853051954/786286026606903326/telechargement.jpeg')"
+	document.body.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/785944479853051954/786286026606903326/telechargement.jpeg')";
+  	alert("Tu as un qi de " + nombre);
   }
 }
